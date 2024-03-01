3e5d3c6c7f99f011d8b0d8af8fb5bfb733eedd6c
Hemershow <hemersonviolin@gmail.com>
2024-03-01 16:31:11 -0300

    Fixed typo

--- controllers/ImageController.js
+++ controllers/ImageController.js
@@ -2,48 +2,48 @@ const multer = require('multer');
 const sharp = require('sharp');
 const crypto = require('crypto');
 const Image = require('../models/Image')
-const { uploadFile, deleteFile, getObjectSignedUrl } = require('../config/s3');
+// const { uploadFile, deleteFile, getObjectSignedUrl } = require('../config/s3');
 
 class ImageController {
-    static generateFileName(bytes = 32) {
-        return crypto.randomBytes(bytes).toString('hex');
-    }
+    // static generateFileName(bytes = 32) {
+    //     return crypto.randomBytes(bytes).toString('hex');
+    // }
 
-    static async Get(req, res) {
+    // static async Get(req, res) {
 
-        const posts = await Image.getPosts(); 
-        for (let post of posts) {
-            post.imageUrl = await getObjectSignedUrl(post.imageName);
-        }
-        res.send(posts);
-    }
+    //     const posts = await Image.getPosts(); 
+    //     for (let post of posts) {
+    //         post.imageUrl = await getObjectSignedUrl(post.imageName);
+    //     }
+    //     res.send(posts);
+    // }
 
-    static async Create(req, res) {
-        const file = req.file;
-        const caption = req.body.caption;
-        const imageName = ImageController.generateFileName();
+    // static async Create(req, res) {
+    //     const file = req.file;
+    //     const caption = req.body.caption;
+    //     const imageName = ImageController.generateFileName();
 
-        const fileBuffer = await sharp(file.buffer)
-            .resize({ height: 1920, width: 1080, fit: "contain" })
-            .toBuffer();
+    //     const fileBuffer = await sharp(file.buffer)
+    //         .resize({ height: 1920, width: 1080, fit: "contain" })
+    //         .toBuffer();
 
-        await uploadFile(fileBuffer, imageName, file.mimetype);
+    //     await uploadFile(fileBuffer, imageName, file.mimetype);
 
-        const post = await Image.createPost({ imageName, caption }); 
+    //     const post = await Image.createPost({ imageName, caption }); 
         
-        res.status(201).send(post);
-    }
+    //     res.status(201).send(post);
+    // }
 
-    static async Delete(req, res) {
-        const id = req.params.id;
+    // static async Delete(req, res) {
+    //     const id = req.params.id;
 
-        const post = await Image.findPostById(id); 
+    //     const post = await Image.findPostById(id); 
 
-        await deleteFile(post.imageName);
+    //     await deleteFile(post.imageName);
 
-        await Image.deletePost(id); 
-        res.send(post);
-    }
+    //     await Image.deletePost(id); 
+    //     res.send(post);
+    // }
 }
 
 module.exports = ImageController;
